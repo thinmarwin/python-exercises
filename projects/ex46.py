a152025fd40b7a8f520885273ece7dd9561a87bf
thinmarwin <thinmarwin951@gmail.com>
2018-06-14 22:00:51 -0700

    add ex46.py

---
+++ projects/ex46.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+    config = {
+            'description' : 'My Project',
+            'author' :'My Name',
+            'url': 'URL to get it at.',
+            'download_url' : 'Where to download it.',
+            'author_email' : 'My email',
+            'version' : '0.1',
+            'install_requires': ['nose'],
+            'packages': ['NAME'],
+            'scripts': [],
+            'name' : 'projectname'
+            }
+
+    setup(**config)
+import NAME
+
+def setup():
+    print("SETUP!")
+
+def teardown():
+    print("TEAR DOWN")
+
+def test_basic():
+    print("I RAN!", end='')
