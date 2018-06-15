46ac8d6e911e844fd137b436f16424a8b2f2ddab
thinmarwin <thinmarwin951@gmail.com>
2018-06-15 04:36:04 -0700

    add ex49(parser.py)

---
+++ parser.py
@@ -0,0 +1,65 @@
+class ParserError(Exception):
+    pass
+
+class Sentence(object):
+
+    def __init__(self, subject, verb, obj):
+        # remember we take ('noun', 'princess') tuples and convert them
+        self.subject = subject[1]
+        self.verb = verb[1]
+        self.obect = obj[1]
+
+    def peek(word_list):
+        if word_list:
+            word = word_list[0]
+            return word[0]
+        else:
+            return None
+
+    def math(word_list, expecting):
+        if word_list:
+            word = word_list.pop[0]
+            return word
+        else:
+            return None
+        else:
+            return None
+
+def skip(word_list, word_type):
+    while peek(word_list) == word_type:
+        match(word_list, word_type)
+
+def parse_verb(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'verb':
+        return match(word_list, 'verb')
+    else:
+        raise ParserError("Expected a verb next.")
+
+def parse_object(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    else:
+        raise ParserError("Expected a noun of direction  next.")
+
+def parse_subject(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'verb':
+        return ('noun', 'player')
+    else:
+        return ParsError("Expected a verb next.")
+
+def parse_sentence(word_list)
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parde_object(word_list)
+
+    return Sentence(subj, verb, obj)
